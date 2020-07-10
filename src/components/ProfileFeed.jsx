f843c5310a4f55cf89d30f5331075f00cecc763c
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-07-10 14:19:20 -0400

    editing feeds / profile boiler plate

---
+++ src/components/ProfileFeed.jsx
@@ -0,0 +1,234 @@
+import React from "react";
+import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
+//import AdSense from 'react-adsense';
+
+const ProfileFeed = () => {
+    //for header if needed : style={{ marginBottom: '100px' }}
+    return (
+            <MDBCard
+
+                className="mb-5 px-5 pt-4 fluid"
+                style={{ fontWeight: 300, maxWidth: 2000 }}
+            >
+                <MDBCardBody className="py-0">
+                    <MDBRow>
+                        <div className="mdb-feed">
+                            <div className="news">
+                                <div className="label">
+                                    <img
+                                        src="https://mdbootstrap.com/img/Photos/Avatars/avatar-1-mini.jpg"
+                                        alt=""
+                                        className="rounded-circle z-depth-1-half"
+                                    />
+                                </div>
+                                <div className="excerpt">
+                                    <div className="brief">
+                                        <a href="#!" className="name">
+                                            Spot
+                                        </a> added you as a friend
+                                        <div className="date">1 hour ago</div>
+                                    </div>
+                                    <div className="feed-footer">
+                                        <a href="#!" className="like">
+                                            <MDBIcon icon="heart" />
+                                            <span>5 likes</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="news">
+                                <div className="label">
+                                    <img
+                                        src="https://mdbootstrap.com/img/Photos/Avatars/img%20(17)-mini.jpg"
+                                        alt=""
+                                        className="rounded-circle z-depth-1-half"
+                                    />
+                                </div>
+                                <div className="excerpt">
+                                    <div className="brief">
+                                        <a href="#!" className="name">
+                                            Heidi
+                                        </a> added <a href="#!">2 new photos</a>
+                                        <div className="date">4 hours ago</div>
+                                    </div>
+                                    <div className="added-images">
+                                        <img
+                                            src="https://mdbootstrap.com/img/Photos/Others/images/71.jpg"
+                                            alt=""
+                                            className="z-depth-1 rounded mb-md-0 mb-2"
+                                        />
+                                        <img
+                                            src="https://mdbootstrap.com/img/Photos/Others/images/74.jpg"
+                                            alt=""
+                                            className="z-depth-1 rounded"
+                                        />
+                                    </div>
+                                    <div className="feed-footer">
+                                        <a href="#!" className="like">
+                                            <MDBIcon icon="heart" />
+                                            <span>18 likes</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="news">
+                                <div className="label">
+                                    <img
+                                        src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9)-mini.jpg"
+                                        alt=""
+                                        className="rounded-circle z-depth-1-half"
+                                    />
+                                </div>
+                                <div className="excerpt">
+                                    <div className="brief">
+                                        <a href="#!" className="name">
+                                            Herschel
+                                        </a> added you as a friend
+                                        <div href="#!" className="date">
+                                            7 hours ago
+                                        </div>
+                                    </div>
+                                    <div className="feed-footer">
+                                        <a href="#!" className="like">
+                                            <MDBIcon icon="heart" />
+                                            <span>11 likes</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="news">
+                                <div className="label">
+                                    <img
+                                        src="https://mdbootstrap.com/img/Photos/Avatars/img%20(18)-mini.jpg"
+                                        alt=""
+                                        className="rounded-circle z-depth-1-half"
+                                    />
+                                </div>
+                                <div className="excerpt">
+                                    <div className="brief">
+                                        <a href="#!" className="name">
+                                            Wilder
+                                        </a> posted on her page
+                                        <div className="date">2 days ago</div>
+                                    </div>
+                                    <div className="added-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        Vero inventore, iste quas libero eius? Vitae sint neque
+                                        animi alias sunt dolor, accusantium ducimus, non placeat
+                                        voluptate.
+                                    </div>
+                                    <div className="feed-footer">
+                                        <a href="#!" className="like">
+                                            <MDBIcon icon="heart" />
+                                            <span>7 likes</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="news">
+                                <div className="label">
+                                    <img
+                                        src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20)-mini.jpg"
+                                        alt=""
+                                        className="rounded-circle z-depth-1-half"
+                                    />
+                                </div>
+                                <div className="excerpt">
+                                    <div className="brief">
+                                        <a href="#!" className="name">
+                                            Kobe
+                                        </a> added 
+                                        <a href="#!"> 2 new photos</a> of you
+                                        <div className="date">3 days ago</div>
+                                    </div>
+                                    <div className="added-images">
+                                        <img
+                                            src="https://mdbootstrap.com/img/Photos/Others/images/29.jpg"
+                                            alt=""
+                                            className="z-depth-1 rounded mb-md-0 mb-2"
+                                        />
+                                        <img
+                                            src="https://mdbootstrap.com/img/Photos/Others/images/31.jpg"
+                                            alt=""
+                                            className="z-depth-1 rounded"
+                                        />
+                                    </div>
+                                    <div className="feed-footer">
+                                        <a href="#!" className="like">
+                                            <MDBIcon icon="heart" />
+                                            <span>53 likes</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="news">
+                                <div className="label">
+                                    <img
+                                        src="https://mdbootstrap.com/img/Photos/Avatars/avatar-1-mini.jpg"
+                                        alt=""
+                                        className="rounded-circle z-depth-1-half"
+                                    />
+                                </div>
+                                <div className="excerpt">
+                                    <div className="brief">
+                                        <a href="#!" className="name">
+                                            Spot
+                                        </a> added you as a friend
+                                        <div className="date">1 hour ago</div>
+                                    </div>
+                                    <div className="feed-footer">
+                                        <a href="#!" className="like">
+                                            <MDBIcon icon="heart" />
+                                            <span>5 likes</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="news">
+                                <div className="label">
+                                    <img
+                                        src="https://mdbootstrap.com/img/Photos/Avatars/img%20(17)-mini.jpg"
+                                        alt=""
+                                        className="rounded-circle z-depth-1-half"
+                                    />
+                                </div>
+                                <div className="excerpt">
+                                    <div className="brief">
+                                        <a href="#!" className="name">
+                                            Heidi
+                                        </a> added <a href="#!">2 new photos</a>
+                                        <div className="date">4 hours ago</div>
+                                    </div>
+                                    <div className="added-images">
+                                        <img
+                                            src="https://mdbootstrap.com/img/Photos/Others/images/71.jpg"
+                                            alt=""
+                                            className="z-depth-1 rounded mb-md-0 mb-2"
+                                        />
+                                        <img
+                                            src="https://mdbootstrap.com/img/Photos/Others/images/74.jpg"
+                                            alt=""
+                                            className="z-depth-1 rounded"
+                                        />
+                                    </div>
+                                    <div className="feed-footer">
+                                        <a href="#!" className="like">
+                                            <MDBIcon icon="heart" />
+                                            <span>18 likes</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </MDBRow>
+                </MDBCardBody>
+            </MDBCard>
+    );
+}
+
+export default ProfileFeed;

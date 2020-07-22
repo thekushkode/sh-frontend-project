18bd95c30f06cb177f842afd319df41a6fcb5b1d
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-07-22 18:49:59 -0400

    Feed issue resolved

--- src/components/PrivateFeed.jsx
+++ src/components/PrivateFeed.jsx
@@ -10,56 +10,6 @@ import PhotoPost from "./PhotoPost";
 
 export default function PrivateFeed(props) {
 
-    // const db = firebase.firestore();
-    // const dispatch = useDispatch();
-    // const feed = useSelector(state => state.feed)
-    // const user = useSelector(state => state.user)
-
-    // useEffect(() => {
-    //     db.collection('Feed').orderBy("timestamp", "desc").limit(100).onSnapshot(
-    //         querySnapshot => {
-    //             let feed = [];
-    //             querySnapshot.forEach(function (doc) {
-    //                 const feedData = {
-    //                     ...doc.data(),
-    //                     docId: doc.id
-    //                 }
-    //                 feed.push(feedData);
-    //             })
-    //             dispatch(setFeed(feed))
-    //         });
-    // }, [])
-
-
-    // return (
-    //     <div>
-    //         <main>
-    //             <MDBRow>
-    //                 <MDBCol>
-    //                     {feed && feed.map((item, index) => {
-    //                         {
-    //                             if (item.FriendID === props.location.slice(props.location.length - 20, props.location.length)) {
-    //                                 { console.log(props.location.slice(props.location.length - 20, props.location.length)) }
-    //                                 { console.log(item.FriendID) }
-    //                                 switch (item.Type) {
-    //                                     case 'Post':
-    //                                         return <Post data={item} />
-    //                                     case 'Friend':
-    //                                         return <FriendPost data={item} />
-    //                                     case 'Photo':
-    //                                         return <PhotoPost data={item} />
-    //                                     default:
-    //                                         break;
-    //                                 }
-    //                             }
-    //                         }
-    //                     })}
-    //                 </MDBCol>
-    //             </MDBRow>
-    //         </main>
-    //     </div>
-    // )
-
     const db = firebase.firestore();
     const dispatch = useDispatch();
     const privateFeed = useSelector(state => state.privateFeed)
